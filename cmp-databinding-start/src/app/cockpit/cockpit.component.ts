4034e1e78166c6e71e7188d379201de26a00b155
sudhanshu shekhar <sudhanshu.shekharr@gmail.com>
2018-01-30 08:47:44 -0800

    feat: Get access to template & DOM using @ViewChild

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import {Component, OnInit, EventEmitter, Output, ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,8 +8,9 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  newServerName = '';
+ // newServerName = ''; commented as we are not using two way binding.
   newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
   constructor() { }
 
   ngOnInit() {
@@ -18,13 +19,14 @@ export class CockpitComponent implements OnInit {
     onAddServer(nameInput: HTMLInputElement) {
       this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
     onAddBlueprint(nameInput: HTMLInputElement) {
+      //this.serverContentInput.nativeElement.value = 'something'; should not use something like this.
       this.blueprintCreated.emit({
         serverName: nameInput.value,
-        serverContent: this.newServerContent
+        serverContent: this.serverContentInput.nativeElement.value
       });
   }
 }
