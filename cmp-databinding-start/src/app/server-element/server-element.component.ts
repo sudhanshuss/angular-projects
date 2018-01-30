7acfdcf0b1388a5f6e87560a02ea75c8b7898b4a
sudhanshu shekhar <sudhanshu.shekharr@gmail.com>
2018-01-29 16:03:40 -0800

    feat: Override the style by using encapsulation

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.None // Emulated is the default and don't need to be specified,other options - None,Native
 })
 export class ServerElementComponent implements OnInit {
   @Input('serverElement') element: {type: string, name: string, content: string};
