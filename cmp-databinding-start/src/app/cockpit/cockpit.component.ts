c40d7146aa0e74d98be74df02e04a8d5c444a656
sudhanshu shekhar <sudhanshu.shekharr@gmail.com>
2018-01-30 08:18:46 -0800

    feat: Use local refernence in template

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -15,15 +15,15 @@ export class CockpitComponent implements OnInit {
   ngOnInit() {
   }
 
-    onAddServer() {
+    onAddServer(nameInput: HTMLInputElement) {
       this.serverCreated.emit({
-      serverName: this.newServerName,
+      serverName: nameInput.value,
       serverContent: this.newServerContent
     });
   }
-    onAddBlueprint() {
+    onAddBlueprint(nameInput: HTMLInputElement) {
       this.blueprintCreated.emit({
-        serverName: this.newServerName,
+        serverName: nameInput.value,
         serverContent: this.newServerContent
       });
   }
