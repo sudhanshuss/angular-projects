60398df5c4143916573cb19405e441c4d37ca681
sudhanshu shekhar <sudhanshu.shekharr@gmail.com>
2018-01-31 10:41:04 -0800

    feat: Life Cyle hook in action

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -22,5 +22,12 @@ export class AppComponent {
        name: blueprintData.serverName,
        content: blueprintData.serverContent
      });
+  }
+
+  onChangeFirst(){
+    this.serverElements[0].name = 'Changed!';
+  }
+  onDestroyFirst(){
+    this.serverElements.splice(0,1);
   }
 }
--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,7 @@
-import {Component, OnInit, Input, ViewEncapsulation} from '@angular/core';
+import {
+  Component, OnInit, Input, ViewEncapsulation, SimpleChanges, OnChanges, DoCheck,
+  AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,11 +9,45 @@ import {Component, OnInit, Input, ViewEncapsulation} from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.None // Emulated is the default and don't need to be specified,other options - None,Native
 })
-export class ServerElementComponent implements OnInit {
-  @Input('serverElement') element: {type: string, name: string, content: string};
-  constructor() { }
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck,
+  AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
+  @Input('serverElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
+  constructor() {
+    console.log('constructor called!');
+  }
+  ngOnChanges(changes: SimpleChanges){
+    console.log('ngOnChanges called!');
+    console.log(changes);
+
+  }
   ngOnInit() {
+    console.log('ngOnInit called!');
+  }
+
+  ngDoCheck(){
+    console.log('ngDoCheck called!');
+  }
+
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit called!');
+  }
+
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked called!');
+  }
+
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit called!');
+  }
+
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked called!');
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy called!');
   }
 
 }
