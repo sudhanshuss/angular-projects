243c0ba5f8ca79e1282dfa50d2b0789443cd86ac
sudhanshu shekhar <sudhanshu.shekharr@gmail.com>
2018-02-02 11:14:34 -0800

    feat: Add ngSwitch directive

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -8,6 +8,8 @@ import {Component, SimpleChanges} from '@angular/core';
 export class AppComponent {
   serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
 
+  value = 5;
+
   onServerAdded(serverData: {serverName: string, serverContent: string}) {
      this.serverElements.push({
        type: 'server',
