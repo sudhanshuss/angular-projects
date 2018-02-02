d8d20d9dba32d8482a92253457424f4b0dfd5bf4
sudhanshu shekhar <sudhanshu.shekharr@gmail.com>
2018-02-02 10:59:39 -0800

    feat: Build a structural directive
    To use structural directive,we need to use with * with the directive name
    eg - *appUnless="onlyOdd"

--- cmp-databinding-start/src/app/app.module.ts
+++ cmp-databinding-start/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
 import {BasicHighlightDirective} from "./basic-highlight/basic-highlight.directive";
 import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+import { UnlessDirective } from './unless/unless.directive';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { BetterHighlightDirective } from './better-highlight/better-highlight.di
     CockpitComponent,
     ServerElementComponent,
     BasicHighlightDirective,
-    BetterHighlightDirective
+    BetterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
