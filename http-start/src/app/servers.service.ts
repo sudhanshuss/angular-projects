992b713166e8226b73d26c69026f36ee2c4295b0
sudhanshu shekhar <sudhanshu.shekharr@gmail.com>
2018-03-22 20:42:30 -0700

    Feat: Http Put request implementation

--- http-start/src/app/servers.service.ts
+++ http-start/src/app/servers.service.ts
@@ -6,7 +6,8 @@ export class ServerService {
   constructor(private http: Http) {}
   storeServers(servers: any[]) {
     const headers = new Headers({'content-Type': 'application/json'});
-    return this.http.post('https://ng-http-efac1.firebaseio.com/data.json', servers, {headers: headers});
+   // return this.http.post('https://ng-http-efac1.firebaseio.com/data.json', servers, {headers: headers});
+    return this.http.put('https://ng-http-efac1.firebaseio.com/data.json', servers, {headers: headers});
   }
   getServers() {
     return this.http.get('https://ng-http-efac1.firebaseio.com/data.json')
