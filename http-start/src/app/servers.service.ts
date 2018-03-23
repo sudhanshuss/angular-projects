ee8e3e781eceedee0f788a9ea44ee36a3e714c6a
sudhanshu shekhar <sudhanshu.shekharr@gmail.com>
2018-03-22 21:11:56 -0700

    Feat: Catch Http Errors

--- http-start/src/app/servers.service.ts
+++ http-start/src/app/servers.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Headers, Http, Response} from '@angular/http';
 import 'rxjs/Rx'
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class ServerService {
@@ -11,13 +12,17 @@ export class ServerService {
     return this.http.put('https://ng-http-efac1.firebaseio.com/data.json', servers, {headers: headers});
   }
   getServers() {
-    return this.http.get('https://ng-http-efac1.firebaseio.com/data.json').map(
+    return this.http.get('https://ng-http-efac1.firebaseio.com/data').map(
       (response: Response) => {
         const data = response.json();
         for(const server of data){
           server.name = 'FETCHED_' + server.name;
         }
         return data;
+      }
+    ).catch(
+      (error: Response) => {
+        return Observable.throw('Something went wrong');
       }
     );
   }
