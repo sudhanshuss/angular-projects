dffa742bf052b728f9a1589c526df4fcbf7bf3ac
sudhanshu shekhar <sudhanshu.shekharr@gmail.com>
2018-03-22 17:44:41 -0700

    Feat: Add Headers to http post request

--- http-start/src/app/servers.service.ts
+++ http-start/src/app/servers.service.ts
@@ -1,10 +1,11 @@
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
+import {Headers, Http} from '@angular/http';
 
 @Injectable()
 export class ServerService {
   constructor(private http: Http) {}
     storeServers(servers: any[]) {
-     return this.http.post('https://ng-http-efac1.firebaseio.com/data.json', servers);
+    const headers = new Headers({'content-Type': 'application/json'});
+     return this.http.post('https://ng-http-efac1.firebaseio.com/data.json', servers, {headers: headers});
     }
 }
